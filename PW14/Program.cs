730795244a4ede304252dda250a603485025f7e0
sarvinduri <s.ramm.s.ramm@gmail.com>
2023-03-29 10:06:05 +0300

    Добавьте файлы проекта.

---
+++ PW14/Program.cs
@@ -0,0 +1,13 @@
+﻿string text = File.ReadAllText("Input.txt");
+
+int count = 0;
+
+    if (text[0] == 'о')
+        count++;
+    for (int i = 1; i < text.Length; i++)
+    {
+        if (text[i] == 'о' && text[i - 1] == ' ')
+            count++;
+    }
+    Console.WriteLine("Количество слов начинающихся на о: " + count);
+    Console.ReadKey();
